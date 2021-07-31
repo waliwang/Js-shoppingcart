3bee6370d04bff6797f27736f70e3a73c884b350
wali <larts393@gmail.com>
2021-07-31 23:49:25 +0800

    ok

--- script.js
+++ script.js
@@ -1,61 +1,68 @@
-const itemCount = () => {
-    return document.querySelectorAll(".list li").length
-}
-
-
-const updateState = () => {
-
-    let statelabel = document.querySelector("#state")
-    let countlabel = document.querySelector("#count")
+function itemList() {
+    return document.querySelectorAll(".list li")
+  }
+  
+  function itemCount() {
+    return itemList().length
+  }
+  
+  function updateState() {
+    let stateLabel = document.querySelector("#state")
+    let countLabel = document.querySelector("#count")
     let count = itemCount()
-
-    countlabel.textContent = count
-
-    if(count === 0){
-        statelabel.textContent = "目前沒有商品"
+  
+    countLabel.textContent = count
+  
+    if (count === 0) {
+      stateLabel.textContent = "目前沒有任何商品"
     } else {
-        statelabel.textContent = " "
+      stateLabel.textContent = ""
     }
-}
-
-const addToCart = (item) => {
+  }
+  
+  const addToCart = (item) => {
     let shoppingList = document.querySelector(".list")
     shoppingList.appendChild(item)
     updateState()
-}
-
-document.addEventListener("DOMContentLoaded",() => {
-    document.querySelector(".list").addEventListener("click",(e) => {
-       if(e.target.className == "closeBtn") {
-           const btn = e.target
-           btn.parentNode.remove()
-           updateState()
-       }
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#emptyCart").addEventListener("click", (e) => {
+      const allItems = itemList()
+  
+      if (allItems.length > 0) {
+        allItems.forEach(item => {
+          item.remove()
+        })
+      }
+      updateState()
     })
-
-    // document.querySelector("#c-123 button").addEventListener("click", (e) => {
-    //     e.target.parentNode.remove()
-    // })
-
-    // document.querySelector("#c-777 button").addEventListener("click", (e) => {
-    //     e.target.parentNode.remove()
-    // })
-
+  
+    document.querySelector(".list").addEventListener("click", (e) => {
+      if (e.target.className === "closeBtn") {
+        e.target.parentNode.remove()
+        updateState()
+      }
+    })
+  
     document.querySelector("#productArea").addEventListener("click", (e) => {
-        e.preventDefault()
-        let link = e.target
-        let data = link.dataset
-        let id = data.productId
-        let shoppingList = document.querySelector(".list")
-
-        if(id){
-            const el = document.createElement("li")
-            const btn = document.createElement("button")
-            btn.textContent = "x"
-            btn.className = "closeBtn"
-            el.textContent = id
-            el.appendChild(btn)
-            addToCart(el)
-        }
+      e.preventDefault()
+  
+      let link = e.target
+      let data = link.dataset
+      let id = data.productId
+  
+      if (id) {
+        const el = document.createElement("li")
+        const btn = document.createElement("button")
+        btn.textContent = "X"
+        btn.className = "closeBtn"
+  
+        el.textContent = id
+        el.appendChild(btn)
+  
+        addToCart(el)
+        // shoppingList.appendChild(el)
+      }
     })
-})
+  })
